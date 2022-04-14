b8c42a32b81027ccd8da4ae6cf7fbf9d7f534b60
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-04-13 20:45:48 -0400

    first poc for unmarshaling the json

--- json/sample.go
+++ json/sample.go
@@ -4,11 +4,11 @@ package main;
 import (
     "fmt"
     "encoding/json"
-    "logs"
+    "log"
     "strings"
 )
 
-Type Animal int
+type Animal int
 
 const (
     Unknown Animal = iota           // what is this crap
@@ -29,9 +29,41 @@ func (a *Animal) UnmarshalJSON (b []byte) error {
          case "zebra":
             *a = Zebra    
      }
-     return nil;
+     return nil
 }
 
+func (a Animal) MarshalJSON()([]byte, error) {
+    var s string
+    switch a {
+        case Unknown:
+            s = "unknown"
+        case Gopher:
+            s = "Gopher"
+        case Zebra:
+            s = "Zebra"
+    }
+    return json.Marshal(s)
+}
 
+func main() {
+    blob := `["gopher","armadillo","zebra","unknown","gopher","bee","gopher","zebra"]`
+    
+    var zoo []Animal
+
+    if err := json.Unmarshal([]byte(blob), &zoo); err != nil {
+        log.Fatal(err)
+    }
+
+    census := make(map[Animal]int)
+
+    for _,animal := range zoo {
+        census[animal] += 1
+    }
+
+    fmt.Printf("Zoo Census:\n\x1B[32m* Gophers: %d\n\x1B[33m* Zebras: %d\n\x1B[0m* Uknown: %d\n", census[Gopher], census[Zebra], census[Unknown])
+
+    //Playing with the marshal 
+
+}
 
 

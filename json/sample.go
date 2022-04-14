3d24d0e2af0b4b7aa96db6d45bb653472f15afb2
saurabh10041998 <shindesaurabhpict@gmail.com>
2022-04-13 20:28:50 -0400

    Unmarshal json method

--- json/sample.go
+++ json/sample.go
@@ -17,8 +17,21 @@ const (
 )
 
 func (a *Animal) UnmarshalJSON (b []byte) error {
-    
-
+     var s string
+     if err := json.Unmarshal(b, &s); err != nil {
+        return err
+     }
+     switch strings.ToLower(s) {
+        default:
+            *a =  Unknown
+        case "gopher":
+            *a = Gopher
+         case "zebra":
+            *a = Zebra    
+     }
+     return nil;
 }
+
+
 
 
